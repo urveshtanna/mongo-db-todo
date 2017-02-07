bf91bbc28c996689ab5029073a33cd70de055e8b
Urvesh Tanna <urvesh.tanna@truce.in>
2017-02-07 09:06:50 +0530

    update db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://urvesh:t^K57wfqJsI@155$@ds145039.mlab.com:45039/todoapp' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {
